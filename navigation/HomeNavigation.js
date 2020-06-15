635418ac36fddac70ebdd41c455a2bae0f84e23a
lcsalt <96.laltamirano@gmail.com>
2020-06-15 18:34:12 -0300

    fix token, add location, request permisos, separado de aplicacion en guia y turista

--- navigation/HomeNavigation.js
+++ navigation/HomeNavigation.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { FontAwesome } from '@expo/vector-icons'; 
@@ -13,12 +14,18 @@ import Mochila from "../screens/home/Mochila.js";
 const Tab = createBottomTabNavigator();
 
 function HomeNavigation() {
-  return (
-    <Tab.Navigator
+  const role = useSelector((state) => state.auth.role);
+
+  
+  useEffect( () => console.log(role));
+  
+  if (role == 'Turista'){
+    return (
+      <Tab.Navigator
       initialRouteName="Mapa"
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
-
+      
           if (route.name === "Perfil") {
             return <FontAwesome name="user-circle-o" size={size} color={color} />;
           } else if (route.name === "Mapa") {
@@ -32,12 +39,43 @@ function HomeNavigation() {
         activeTintColor: colors.PRIMARY,
         inactiveTintColor: "gray",
       }}
-    >
+      >
       <Tab.Screen name="Perfil" component={Perfil} />
       <Tab.Screen name="Mapa" component={Home} />
       <Tab.Screen name="Mochila" component={Mochila}  />
-    </Tab.Navigator>
-  );
+      </Tab.Navigator>
+    );
+  }
+   
+  return (
+      <Tab.Navigator
+      initialRouteName="Mapa"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+      
+          if (route.name === "Perfil") {
+            return <FontAwesome name="user-circle-o" size={size} color={color} />;
+          } else if (route.name === "Mapa") {
+            return <Feather name="map" size={size} color={color} />;
+          } else if (route.name === "Mochila") {
+            return <MaterialCommunityIcons name="bag-personal-outline" size={size} color={color} />
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: colors.PRIMARY,
+        inactiveTintColor: "gray",
+      }}
+      >
+      <Tab.Screen name="Perfil" component={Perfil} />
+      <Tab.Screen name="Mapa" component={Home} />
+      <Tab.Screen name="Mochila" component={Mochila}  />
+      </Tab.Navigator>   
+    );
+  
+  
 }
 
+
 export default HomeNavigation;
+
